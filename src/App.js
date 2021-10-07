d14b771891f96f1ceeac199317a18ff94ea5508b
Pyunghyuk Yoo <yoophi@gmail.com>
2021-10-08 00:47:28 +0900

    ✨ UPDATE: client now calls dummy /coins api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { API } from "aws-amplify";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [coins, updateCoins] = useState([]);
+  async function fetchCoins() {
+    const data = await API.get("cryptoapi", "/coins");
+    updateCoins(data.coins);
+  }
+  useEffect(() => {
+    fetchCoins();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {coins.map((coin, index) => (
+        <div key={index}>
+          <h1>
+            {coin.name} - {coin.symbol}
+          </h1>
+          <h5>${coin.price_usd}</h5>
+        </div>
+      ))}
     </div>
   );
 }
