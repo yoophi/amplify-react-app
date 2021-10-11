cdfc9f487dd4cadc8054392a712fae407b53d846
Pyunghyuk Yoo <yoophi@gmail.com>
2021-10-11 23:00:27 +0900

    ✨ UPDATE: frontend to get start, limit input

--- src/App.js
+++ src/App.js
@@ -3,17 +3,44 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [input, updateInput] = useState({ limit: 5, start: 0 });
   const [coins, updateCoins] = useState([]);
+
+  function updateInputValue(type, value) {
+    updateInput({ ...input, [type]: value });
+  }
+
   async function fetchCoins() {
-    const data = await API.get("cryptoapi", "/coins");
+    const { limit, start } = input;
+    const data = await API.get(
+      "cryptoapi",
+      `/coins?limit=${limit}&start=${start}`
+    );
     updateCoins(data.coins);
   }
+
   useEffect(() => {
     fetchCoins();
   }, []);
 
   return (
     <div className="App">
+      <div>
+        <input
+          onChange={(e) => updateInputValue("limit", e.target.value)}
+          placeholder="limit"
+        />
+      </div>
+      <div>
+        <input
+          onChange={(e) => updateInputValue("start", e.target.value)}
+          placeholder="start"
+        />
+      </div>
+      <div>
+        <button onClick={fetchCoins}>Fetch Coins</button>
+      </div>
+
       {coins.map((coin, index) => (
         <div key={index}>
           <h1>
